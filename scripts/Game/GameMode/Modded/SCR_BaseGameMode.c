32cbf87cf6953503ab13119681c82471613b7dc7
tsanvr <tsanvr@outlook.jp>
2025-07-17 22:14:57 +0900

    first commit

---
+++ scripts/Game/GameMode/Modded/SCR_BaseGameMode.c
@@ -0,0 +1,22 @@
+modded class SCR_BaseGameMode
+{
+	override void OnGameStart()
+	{
+		super.OnGameStart();
+		if (Replication.IsServer())
+		{
+			Print ("Init: TSAN_GarbageCollect()");
+			GetGame().GetCallqueue().CallLater(TSAN_GarbageCollect,900000,true);
+		}		
+	}
+	private void TSAN_GarbageCollect()
+	{
+		Print ("Start: Execute TSAN_GarbageCollect()"); 
+		ChimeraWorld chimeraWorld = GetGame().GetWorld();
+		if (!chimeraWorld) return;	
+		GarbageSystem garbageSystem = GarbageSystem.Cast(chimeraWorld.FindSystem(GarbageSystem));
+		if (!garbageSystem) return;
+		garbageSystem.Flush();
+		Print ("End: Execute TSAN_GarbageCollect()");
+	}
+}
---
+++ scripts/Game/GameMode/Modded/SCR_GameModeCampaign.c
@@ -0,0 +1,48 @@
+//modded class SCR_GameModeCampaign
+//{	
+//	//この上限に達するまで、基地の補給品が自動的に補充されます。
+//	// 1000
+//	override int GetSuppliesReplenishThreshold()
+//	{
+//		return 600;
+//	}
+//	//この上限に達するまで、基地の補給品が迅速に自動補充されます（司令部は影響を受けません）。
+//	// 200
+//	override int GetQuickSuppliesReplenishThreshold()
+//	{
+//		return 400;
+//	}
+//	//司令部に定期的に補充される補給品の数.
+//	// 40
+//	override int GetRegularSuppliesIncome()
+//	{
+//		return 50;
+//	}
+//	
+//	//司令部以外の基地に定期的に補充される補給品の数。
+//	// 4
+//	override int GetRegularSuppliesIncomeBase()
+//	{
+//		return 10;
+//	}
+//	
+//	//無線で接続された基地ごとに、司令部以外の基地に定期的に補充される追加の補給品の数。
+//	// 4
+//	override int GetRegularSuppliesIncomeExtra()
+//	{
+//		return 4;
+//	}
+//	
+//	// 100
+//	override int GetMinStartingSupplies()
+//	{
+//		return 600;
+//	}
+//
+//	// 500
+//	override int GetMaxStartingSupplies()
+//	{
+//		return 600;
+//	}
+//}
+
